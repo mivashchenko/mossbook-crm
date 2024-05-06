719dff1b2d1e2415445f5484a396c7bb26694321
maksymivashchenko <Maksym.Ivashchenko@andorhealth.com>
2024-05-06 16:05:18 -0400

    update landing page

--- components/landing/cta.tsx
+++ components/landing/cta.tsx
@@ -7,12 +7,12 @@ import { Card } from '@/components/ui/card'
 const Cta = () => {
   return (
     <Container>
-      <Card className='mx-auto flex w-full max-w-4xl flex-wrap items-center justify-between gap-5 rounded-xl px-7 py-7 text-white lg:flex-nowrap lg:px-12 lg:py-12'>
+      <Card className='mx-auto flex w-full max-w-4xl flex-wrap items-center justify-between gap-5 rounded-xl px-7 py-7  lg:flex-nowrap lg:px-12 lg:py-12'>
         <div className='flex-grow text-center lg:text-left'>
           <h2 className='text-2xl font-medium lg:text-3xl'>
             Curious to Experience Mossbook?
           </h2>
-          <p className='mt-2 font-medium text-white text-opacity-90 lg:text-xl'>
+          <p className='mt-2 font-medium lg:text-xl'>
             Don&apos;t let your business fall behind – give your clients a
             seamless booking experience today!
           </p>
